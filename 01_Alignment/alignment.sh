233362592f3dbfe2ea5a20496347c93229f12260
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-23 20:12:11 -0600

    alignment.sh

--- 01_Alignment/alignment.sh
+++ 01_Alignment/alignment.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 1. Create variables for directories.
-ref_alignment=/home/path/to/dir/bwa_index
-source=/home/path/to/dir/source
-
-# 2. Load modules.
+# 1. Load modules.
 module load gatk/4.1.2.0
 module load java/13.0.1
 module load bwa/0.7.17
 module load samtools/1.10
+
+# 2. Create variables for directories.
+ref_alignment=/home/path/to/dir/bwa_index
+source=/home/path/to/dir/source
 
 # 3. Alignment with bwa mem. The output is a sam file.
 srun bwa mem -M -t 26 -p $ref_alignment/Homo_sapiens.hg19.fa $source/bam1_interleaved.fq > $source/bam1_aln.sam
