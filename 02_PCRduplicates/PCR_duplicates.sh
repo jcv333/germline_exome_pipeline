45ebec473145a444976ba9c76b72e8f51d9989b9
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-23 20:13:11 -0600

    PCR_duplicates.sh

--- 02_PCRduplicates/PCR_duplicates.sh
+++ 02_PCRduplicates/PCR_duplicates.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 1. Create variables for directories.
-source=/home/path/to/dir/source
-
-# 2. Load modules.
+# 1. Load modules.
 module load gatk/4.1.2.0
 module load java/13.0.1
 module load bwa/0.7.17
 module load samtools/1.10
+
+# 2. Create variables for directories.
+source=/home/path/to/dir/source
 
 # 3. Sort the aligned bam file.
 srun gatk --java-options "-Xms30g -Xmx120g" SortSam \
