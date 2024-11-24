35b10024fae5e9fe2ccf4d1cdb26361da62507d9
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-23 19:23:17 -0600

    variant_calling.sh

---
+++ 03_VariantCalling/variant_calling.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. Create variables for directories.
+ref=/home/path/to/dir/hg19_reference
+source=/home/path/to/dir/source
+
+# 2. Load modules.
+module load gatk/4.1.2.0
+module load java/1.8.0_192
+module load samtools/1.10
+
+# 3. Variant calling - haplotypecaller.
+srun gatk --java-options "-Xms30g -Xmx120g" HaplotypeCaller \
+-R $ref/Homo_sapiens.hg19.fa \
+-I $source/bam1_sorted_nodup_aln.bam \
+-O $source/bam1_nodup.hc.raw.vcf.gz \
+--standard-min-confidence-threshold-for-calling 20
+
+# 4. Remove no longer useful intermediate files.
+rm $source/bam1_sorted_nodup_aln.bam
+
+# The end.
