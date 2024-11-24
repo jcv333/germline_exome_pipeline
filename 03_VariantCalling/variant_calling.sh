3626683d7e87a6d8a6f296869453302bdd7244ba
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-23 20:13:56 -0600

    variant_calling.sh

--- 03_VariantCalling/variant_calling.sh
+++ 03_VariantCalling/variant_calling.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 1. Create variables for directories.
-ref=/home/path/to/dir/hg19_reference
-source=/home/path/to/dir/source
-
-# 2. Load modules.
+# 1. Load modules.
 module load gatk/4.1.2.0
 module load java/1.8.0_192
 module load samtools/1.10
+
+# 2. Create variables for directories.
+ref=/home/path/to/dir/hg19_reference
+source=/home/path/to/dir/source
 
 # 3. Variant calling - haplotypecaller.
 srun gatk --java-options "-Xms30g -Xmx120g" HaplotypeCaller \
