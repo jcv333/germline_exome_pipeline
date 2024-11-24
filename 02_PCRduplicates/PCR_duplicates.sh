80160d2d30fbd5a6e3db5c7115f8a41003ab4175
Jose Camacho Valenzuela <jcamachov_23@hotmail.com>
2024-11-23 19:26:33 -0600

    PCR_duplicates.sh

--- 02_PCRduplicates/PCR_duplicates.sh
+++ 02_PCRduplicates/PCR_duplicates.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# 1. Variables for directories.
+# 1. Create variables for directories.
 source=/home/path/to/dir/source
 
-# 2. Sort the aligned bam file.
+# 2. Load modules.
+module load gatk/4.1.2.0
+module load java/13.0.1
+module load bwa/0.7.17
+module load samtools/1.10
+
+# 3. Sort the aligned bam file.
 srun gatk --java-options "-Xms30g -Xmx120g" SortSam \
 -I $source/bam1_mrg_aln.bam \
 -O $source/bam1_sorted_aln.bam \
 --SORT_ORDER coordinate
 
-# 3. Mark PCR duplicates.
+# 4. Mark PCR duplicates.
 srun gatk --java-options "-Xms30g -Xmx120g" MarkDuplicates \
 -I $source/bam1_sorted_aln.bam \
 -O $source/bam1_sorted_nodup_aln.bam \
 -M $source/bam1_sorted_nodup_aln_metrics.txt \
 --REMOVE_DUPLICATES true
 
-# 4. Remove no longer useful intermediate files.
+# 5. Remove no longer useful intermediate files.
 rm $source/bam1_mrg_aln.bam
 rm $source/bam1_mrg_aln.bai
 
